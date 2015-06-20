6456632dee040b6840f282a7b9f5f42afe9760ec
Jamen Marz <jamen.marz@gmail.com>
2015-06-20 15:24:07 -0600

    Added .loud folder creation.

--- packages/loudnode/lib/config.js
+++ packages/loudnode/lib/config.js
@@ -1,5 +1,15 @@
-var Config = function(lib){
+var Config = function(lib, params){
 
+  // Specified directory, or default?
+  var base = (params.configPath || (process.env.HOME || process.env.APPDATA)) + "/.loud";
+
+  // Check if directory exists, if not, create dat shit.
+  lib.fs.exists(base, function(exists){
+    if (exists) return;
+    lib.fs.mkdir(base, 0777, function(err){
+      if (err) console.log("LoudNode: Failed to make directory at " + base + " (insufficient permissions?)")
+    });
+  });
 };
 
 module.exports = exports = Config;
