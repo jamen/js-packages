f55e99ec8b0a646c49c25451400a8eb490ec0d06
JamenMarz <jamen.marz@gmail.com>
2015-09-05 20:00:28 -0600

    Added reader's functionality

--- packages/refig/lib/reader.js
+++ packages/refig/lib/reader.js
@@ -1,7 +1,8 @@
 (function(fs, path){
-  var Reader = function(options){
+  var Reader = function(options, parser){
     if (!(this instanceof Reader)) return new Reader(options);
     this.options = (typeof options === 'object') ? options : {};
+    this.parser = (typeof parser !== 'undefined') ? parser : {};
     return this;
   };
 
@@ -12,10 +13,33 @@
     return this;
   };
 
-  Reader.prototype.read = function(filename, async){
-    return (function(options){
+  Reader.prototype.read = function(filepath, async){
+    return (function(options, parser){
+      var isAsync = true;
+      if (typeof options.async !== 'undefined') isAsync = options.async;
 
-    })(this.options);
+      var base = '';
+      if (typeof options.base !== 'undefined') base = options.base;
+
+      filepath = (function(){
+        if (!path.isAbsolute(filepath)) {
+          return path.normalize(base + '/' + filepath);
+        } else {
+          return filepath;
+        }
+      })();
+
+      if (isAsync) {
+        fs.readFile(filepath, function(err, data){
+          async(err, data ? parser.parse(data) : data);
+        });
+      } else {
+        return parser.parse(fs.readFileSync(filepath));
+      }
+    })(
+      this.options,
+      this.parser
+    );
   };
 
   module.exports = exports = Reader;
