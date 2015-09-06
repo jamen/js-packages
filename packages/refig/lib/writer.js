da4e13fe2fac8930921ac3101de5b88492f8e7af
JamenMarz <jamen.marz@gmail.com>
2015-09-05 21:15:38 -0600

    Added writing functionality

--- packages/refig/lib/writer.js
+++ packages/refig/lib/writer.js
@@ -1,7 +1,8 @@
 (function(fs, path){
-  var Writer = function(options){
+  var Writer = function(options, parser){
     if (!(this instanceof Writer)) return new Writer(options);
     this.options = (typeof options === 'object') ? options : {};
+    this.parser = (typeof parser !== 'undefined') ? parser : {};
     return this;
   };
 
@@ -12,12 +13,32 @@
     return this;
   };
 
-  Writer.prototype.write = function(filename, async){
-    return (function(options){
-      if (options.example) {
-        console.log('Yey!')
+  Writer.prototype.write = function(filepath, data, async){
+    return (function(options, parser){
+      var isAsync = true;
+      if (typeof options.async !== 'undefined') isAsync = options.async;
+
+      var base = '';
+      if (typeof options.base !== 'undefined') base = options.base;
+
+      filepath = (function(){
+        if (!path.isAbsolute(filepath)) {
+          return path.normalize(base + '/' + filepath);
+        } else {
+          return filepath;
+        }
+      })();
+
+      if (isAsync) {
+        fs.writeFile(filepath, parser.serialize(data, options.indent), async);
+        return this;
+      } else {
+        return fs.writeFileSync(filepath, parser.serialize(data, options.indent));
       }
-    })(this.options);
+    })(
+      this.options,
+      this.parser
+    );
   };
 
   module.exports = exports = Writer;
