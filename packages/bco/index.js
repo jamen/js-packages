654a1c302bf4592cd715d1d1a34102aa6474d317
JamenMarz <jamen.marz@gmail.com>
2015-09-14 23:53:20 -0600

    A small start.

--- packages/bco/index.js
+++ packages/bco/index.js
@@ -1,3 +1,47 @@
+(function(factory){
+
+  if (typeof module === 'object' && typeof module.exports === 'object') {
+    module.exports = factory;
+  } else {
+    window.console = window.bco = factory;
+  }
+
+})(function(){
+
 if (console) {
-  
+  // Clone methods
+  var _console = {};
+  for (var name in console) {
+    _console[name] = console[name];
+  }
+
+  // Make original methods chainable:
+  [
+    'assert', 'count', 'debug',
+    'dir', 'dirxml', 'error',
+    'error', '_exception', 'group',
+    'groupCollapsed', 'groupEnd', 'info',
+    'log', 'profile', 'profileEnd',
+    'table', 'time', 'timeEnd',
+    'timeStamp', 'trace', 'warn'
+  ].forEach(function(name){
+    if (typeof console[name] !== 'undefined') {
+      console[name] = function(){
+        _console[name].apply(this, arguments);
+        return this;
+      };
+    }
+  });
+
+  // Allow access to original methods:
+  console.console = _console;
+
+  // Node-specific
+  if (typeof window === 'undefined') {
+    console.log(console);
+  }
 }
+
+return console;
+
+}());
