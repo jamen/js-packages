78a9a80e0da98525325eea7c2fecab63193ea5b1
JamenMarz <jamen.marz@gmail.com>
2015-09-15 20:57:01 -0600

    Added support for relative paths.

--- packages/iora-static/index.js
+++ packages/iora-static/index.js
@@ -2,11 +2,16 @@
  * iora-static: Implement a static aspect to a portion of your server.
  */
 
-module.exports = function(app, config, express){
+module.exports = function(app, config, iora, express){
+  var path = require('path');
   if (config.static && config.base.static) {
+    var base = config.base.static;
+    if (!path.isAbsolute(base)) {
+      base = path.join(iora.dir, base);
+    }
     app.use(
       config.static.url || '/static',
-      express.static(config.base.static, config.static.options)
+      express.static(base, config.static.options)
     );
   }
 };
