1eaa3d3ce9dd5554639f5a1c1212778892ad8524
Jamen Marz <jamen.marz@gmail.com>
2015-11-02 12:08:57 -0700

    Add decoding of first byte and mask.

--- packages/rela/lib/decode.js
+++ packages/rela/lib/decode.js
@@ -1,5 +1,13 @@
 'use strict';
 
-module.exports = function(){
+module.exports = function(bin){
+  let data = {};
+  data.fin = !!(bin[0] >>> 7);
+  data.reserves = ((bin[0] >>> 4) - (data.fin ? 8 : 0));
+  data.opts = bin[0] - (data.fin ? 128 : 0) - data.reserves;
+  data.masked = !!(bin[1] >>> 7);
 
+  console.log(data);
+
+  return data;
 };
