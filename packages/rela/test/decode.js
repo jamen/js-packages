beca10baca6afd11bb2701142ea5264d0db42117
Jamen Marz <jamen.marz@gmail.com>
2015-11-02 10:37:31 -0700

    Add fake message

--- packages/rela/test/decode.js
+++ packages/rela/test/decode.js
@@ -3,7 +3,7 @@
 module.exports = function(){
   // Unchanging
   const decode = require('../lib').decode,
-        clientMessage = '';
+        clientMessage = new Buffer([129,140,163,113,103,158,235,20,11,242,204,81,16,241,209,29,3,191]);
 
   let decodedMessage = '';
 
