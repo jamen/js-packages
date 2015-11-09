2d6b30fa10e45d938ce2a05a8b5b6dc657b91103
Jamen Marz <jamen.marz@gmail.com>
2015-11-08 20:08:15 -0700

    Remove old tests

--- packages/refig/test/write/write-json.js
+++
@@ -1,6 +0,0 @@
-var refig = require('../..'),
-    testfile = __dirname + '/write-json.json';
-
-refig.write(testfile, {'test': 1},function(){
-  refig.set('indent', 0).write(testfile, {'test': 2});
-});
