5517f0fd4f9070d65b7de040ac29e8191db8e471
Jamen Marz <jamenmarz@gmail.com>
2015-12-11 12:08:44 -0700

    Restart

--- packages/rela/lib/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-module.exports = {
-  'decode': require('./decode'),
-  'encode': require('./encode'),
-  'Server': require('./server'),
-  'Client': require('./client')
-};
--- packages/rela/test/encode.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = function(){
-  const encode = require('../lib/encode');
-
-  console.log(encode('test'));
-};
