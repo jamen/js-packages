9cd0877838e2bdc19e24a6299767572e1f1fb848
Jamen Marz <jamenmarz@gmail.com>
2015-12-11 12:31:32 -0700

    Better index.js layout.

--- packages/rela/index.js
+++ packages/rela/index.js
@@ -1,1 +1,20 @@
-module.exports = require('./lib');
+'use strict';
+
+const Rela = require('./lib'),
+      info = require('./package.json');
+
+/* index.js
+ * Library initialization and package information.
+ * * */
+
+// Initialize new library.
+module.exports = exports = new Rela();
+
+// Raw object accessor.
+exports.Rela = Rela;
+
+// Meta
+exports.name = info.name;
+exports.version = info.version;
+exports.author = info.author;
+exports.license = info.license;
