8a2eb849246cc8c641c016a8beac5d453909216d
Jamen Marz <jamenmarz@gmail.com>
2015-12-11 12:31:42 -0700

    Add basic library object.

--- packages/rela/lib/index.js
+++ packages/rela/lib/index.js
@@ -1,3 +1,18 @@
-module.exports = {
-  
-};
+'use strict';
+
+/* lib/index.js
+ * Library object.
+ * * */
+
+// Export library
+module.exports = Rela;
+
+function Rela(){
+  // Shorthand, no "new" required.
+  if (!(this instanceof Rela))
+    return new Rela(...arguments);
+
+}
+
+// Add EventEmitter functionality to all Rela instances.
+Rela.prototype = require('events').EventEmitter;
