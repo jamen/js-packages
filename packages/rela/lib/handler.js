2753751ac6c6db1b2b973b77996524dbe75552aa
Jamen Marz <jamenmarz@gmail.com>
2015-12-12 00:30:27 -0700

    Add decoding

--- packages/rela/lib/handler.js
+++ packages/rela/lib/handler.js
@@ -1,6 +1,7 @@
 'use strict';
 
-const Client = require('./client');
+const Client = require('./client'),
+      decode = require('./decode');
 
 /* lib/handler.js
  * Handles net socket connections for rela.
@@ -10,4 +11,8 @@ module.exports = function(socket){
   let client = new Client(socket);
   this.clients.push(client);
   this.emit('connection', client);
+
+  client._socket.on('data', (data) => {
+    let ws = decode(data);
+  });
 };
