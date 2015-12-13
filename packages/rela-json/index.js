37cc7e98cc4b2d6dcb2ccf4f68ef32baefca378e
Jamen Marz <jamenmarz@gmail.com>
2015-12-12 23:33:36 -0700

    Add composition.

--- packages/rela-json/index.js
+++ packages/rela-json/index.js
@@ -3,3 +3,25 @@
 /* rela-json
  * JSON-event middleware for rela.
  * * */
+
+module.exports = function(server, Server){
+  let key = 'method';
+  Server.prototype.select = function(other){
+    key = other;
+  };
+
+  server.on('connection', function(client){
+    client.on('text', function(message){
+      let parsed = null;
+      try {
+        parsed = JSON.parse(message);
+      } catch (e) {
+        server.emit('invalid json', message, client);
+        return;
+      }
+
+      if (typeof parsed[key] === 'undefined') client.emit(parsed[key], parsed);
+      else client.emit('_nokey', parsed);
+    });
+  });
+};
