c75978df0cf471074e6dacf2c9987e8db2b1e05d
Jamen Marz <jamenmarz@gmail.com>
2015-12-12 22:58:19 -0700

    Add decoding and opts.

--- packages/rela/lib/message.js
+++ packages/rela/lib/message.js
@@ -1,9 +1,31 @@
 'use strict';
 
+const decode = require('./decode');
+
 /* lib/message.js
  * Handle websocket messages appropriately
  * * */
 
 module.exports = function(data){
+  let message = decode(data);
+
 
 };
+
+exports._opts = [
+  'stream',
+  'text',
+  'binary',
+  '_reserved',
+  '_reserved',
+  '_reserved',
+  '_reserved',
+  'close',
+  'ping',
+  'pong',
+  '_reserved',
+  '_reserved',
+  '_reserved',
+  '_reserved',
+  '_reserved'
+];
