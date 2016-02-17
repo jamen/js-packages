1f259298965ca22f4e4937ac13b3c8de3bfaf5ac
Jamen Marzonie <jamenmarz@gmail.com>
2016-02-17 04:14:53 -0700

    Add JSDoc

--- packages/refig/src/index.js
+++ packages/refig/src/index.js
@@ -5,6 +5,9 @@ import promisify from './promisify';
 
 const [ open, read, stat ] = promisify(fs.open, fs.read, fs.stat);
 
+/** @class Refig
+  * @param {Object} opts - Options
+  */
 export default class Refig {
   constructor(opts) {
     this._opts = new Map();
@@ -15,6 +18,10 @@ export default class Refig {
     for (let opt in opts) this.set(opt, opts[opt]);
   }
 
+  /** Read and parse a configuration file.
+    * @method Refig.load
+    * @param {Array|String} path - A string (or array of strings) of a path.
+    */
   load(path) {
     if (Symbol.iterator in Object(path) && typeof path !== 'string') {
       let master = [];
@@ -22,7 +29,9 @@ export default class Refig {
       return Promise.all(master);
     }
 
-    if (this._cache.has(path)) resolve(this._cache.get(path));
+    if (this._cache.has(path)) {
+      return Promise.resolve(this._cache.get(path));
+    }
 
     return stat(path)
     .then(([ stats ]) => {
@@ -35,27 +44,40 @@ export default class Refig {
     .then(([ x, data ]) => this._get('parser')(data));
   }
 
-  merge(...paths) {
-    return this.load(paths).then(all => {
+  /** Fetch multiple configurations and merge them.
+    * @method Refig.merge
+    * @param {Array|String} path - A string (or array of strings) of a path.
+    */
+  merge(path) {
+    return this.load(path).then(all => {
       let flat = [];
       this._treeRecursion([all], item => flat.push(item));
       return assign({}, ...flat);
     });
   }
 
-  pick(files, name) {
-    return this.load(files).then(all => {
+  /** Pick a single attribute out of a file.
+    * @method Refig.pick
+    * @param {Array|String} path - A string (or array of strings) of a path.
+    * @param {String} name - Name of the attribute to pick.
+    */
+  pick(path, name) {
+    return this.load(path).then(all => {
       let picks = [];
       this._treeRecursion([all], item => picks.push(item[name]));
       return picks;
     });
   }
 
-  purge(item) {
+  /** Purge a file from the cache.
+    * @method Refig.purge
+    * @param {Array|String} path - A string (or array of strings) of a path.
+    */
+  purge(path) {
     let old;
-    if (typeof item !== 'undefined') {
-      old = this._cache.get(item);
-      this._cache.remove(item);
+    if (typeof path !== 'undefined') {
+      old = this._cache.get(path);
+      this._cache.remove(path);
     } else {
       old = this._cache;
       this._cache = new Map();
@@ -63,9 +85,13 @@ export default class Refig {
     return old;
   }
 
+  /** Set an option
+    * @method Refig.set
+    * @alias Refig._opts.set
+    */
   set(...args) { return this._opts.set(...args); }
-  _get(...args) { return this._opts.get(...args); }
 
+  _get(...args) { return this._opts.get(...args); }
   _treeRecursion(input, operation) {
     if (Array.isArray(input)) {
       for (let item of input) this._treeRecursion(item, operation);
