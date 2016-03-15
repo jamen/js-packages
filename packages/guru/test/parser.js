3489012ebd54079240ef17bfcf7959d2bd85d1ce
Jamen Marzonie <jamenmarz@gmail.com>
2016-03-15 05:45:52 -0600

    Add stashing test and update parsing

--- packages/guru/test/parser.js
+++ packages/guru/test/parser.js
@@ -2,7 +2,16 @@ import test from 'ava';
 import Token from '../lib/token';
 import Parser from '../lib/parser';
 
-test('Parsing', t => {
+test('stashing', t => {
+  const foo = new Parser();
+
+  foo.stash('hello', 'world');
+  t.is(foo.stash('hello'), 'world');
+  foo.trash('hello');
+  t.is(typeof foo.stash('hello'), 'undefined');
+});
+
+test('parsing', t => {
   function foo(reader) {
     if (reader.current() === '0') {
       reader.next();
@@ -22,9 +31,8 @@ test('Parsing', t => {
   const foobar = new Parser([foo, bar]);
 
   t.plan(1);
-
-  return foobar.parse('001011').then(() => {
-    t.same(foobar.tokens, [
+  return foobar.parse('001011').then(([tokens]) => {
+    t.same(tokens, [
       new Token('foo', 0),
       new Token('foo', 0),
       new Token('bar', 1),
