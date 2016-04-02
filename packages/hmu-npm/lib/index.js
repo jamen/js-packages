8f79f0f63f7a116c62caf3fcfc6610124ae592ae
Jamen Marzonie <jamenmarz@gmail.com>
2016-04-02 06:47:45 -0600

    Better logging

--- packages/hmu-npm/lib/index.js
+++ packages/hmu-npm/lib/index.js
@@ -1,10 +1,14 @@
 'use strict';
 
 const plugin = require('hmu-plugin')('npm');
+const c = plugin.c;
 
 module.exports = function npm(packs) {
+  plugin.title();
   packs.forEach(pack => {
     plugin.status(`http://registry.npmjs.org/${pack}`, 404)
-    .then(available => plugin.log(available ? 'available' : 'taken'), plugin.error);
+    .then(available => {
+      plugin.log(`${pack} ${available ? c.green('available') : c.red('taken')}`);
+    }, plugin.error);
   });
 };
