81913dfb5b71abd21745a87ad464074b7a7bfdd8
Jamen Marzonie <jamenmarz@gmail.com>
2016-04-02 20:42:02 -0600

    Minor updates

--- packages/hmu-npm/README.md
+++ packages/hmu-npm/README.md
@@ -1,6 +1,8 @@
 # hmu-npm
 > A hmu plugin for checking npm package availability
 
+This is a plugin for [`hmu`][hmu] to check npm package name availability on the [npm registry][npm].
+
 ## Installation
 ```shell
 $ npm install -g hmu-npm
@@ -18,3 +20,5 @@ $ hmu npm <...packages>
 
   [avatar]: https://avatars.githubusercontent.com/u/6251703?v=3&s=125
   [github]: https://github.com/jamen
+  [hmu]: https://github.com/jamen/hmu
+  [npm]: https://npmjs.com/
--- packages/hmu-npm/lib/index.js
+++ packages/hmu-npm/lib/index.js
@@ -4,11 +4,10 @@ const plugin = require('hmu-plugin')('npm');
 const c = plugin.c;
 
 module.exports = function npm(packs) {
-  plugin.title();
   packs.forEach(pack => {
     plugin.status(`http://registry.npmjs.org/${pack}`, 404)
-    .then(available => {
-      plugin.log(`${pack} ${available ? c.green('available') : c.red('taken')}`);
+    .then(free => {
+      plugin.log(`${pack} ${free ? c.green('free') : c.red('taken')}`);
     }, plugin.error);
   });
 };
