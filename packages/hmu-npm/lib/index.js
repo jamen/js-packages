23051bca1085171347a09fa24213f714bd21286d
Jamen Marzonie <jamenmarz@gmail.com>
2016-04-08 06:58:00 -0600

    Update to return promise

--- packages/hmu-npm/lib/index.js
+++ packages/hmu-npm/lib/index.js
@@ -1,13 +1,15 @@
 'use strict';
 
 const plugin = require('hmu-plugin')('npm');
+const routine = require('promise-routine');
 const c = plugin.c;
 
 module.exports = function npm(packs) {
-  packs.forEach(pack => {
-    plugin.status(`http://registry.npmjs.org/${pack}`, 404)
-    .then(free => {
+  return routine(plugin.status, packs.map(n => [`http://registry.npmjs.org/${n}`, 404]), plugin)
+  .then(statuses => {
+    packs.forEach((pack, i) => {
+      const free = statuses[i];
       plugin.log(`${pack} ${free ? c.green('free') : c.red('taken')}`);
-    }, plugin.error);
-  });
+    });
+  }, plugin.error);
 };
