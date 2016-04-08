18adf3fb16c9234684cf5f5635e7e6d0b66fba15
Jamen Marzonie <jamenmarz@gmail.com>
2016-04-08 07:00:39 -0600

    Update to return promise

--- packages/hmu-gh/lib/index.js
+++ packages/hmu-gh/lib/index.js
@@ -3,16 +3,25 @@
 const plugin = require('hmu-plugin')('github');
 const c = plugin.c;
 const https = require('https');
+const routine = require('promise-routine');
 
 module.exports = function gh(searches) {
-  searches.forEach(search => {
-    plugin.status({
-      host: 'api.github.com',
-      path: `/users/${search}`,
-      headers: { 'User-Agent': 'hmu-gh - github.com/jamen/hmu-gh' },
-    }, 404, https).then(
-      free => plugin.log(`${search} ${free ? c.green('free') : c.red('taken')}`),
-      console.error
-    );
-  });
+  return routine(
+    plugin.status,
+    searches.map(search => ([
+      {
+        host: 'api.github.com',
+        path: `/users/${search}`,
+        headers: { 'User-Agent': 'hmu-gh - github.com/jamen/hmu-gh' },
+      },
+      404,
+      https,
+    ])),
+    plugin
+  ).then(statuses => {
+    searches.forEach((search, i) => {
+      const free = statuses[i];
+      plugin.log(`${search} ${free ? c.green('free') : c.red('taken')}`);
+    });
+  }, plugin.error);
 };
