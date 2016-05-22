88024ca909d4268899ab46e8a6417610831799a6
Jamen Marzonie <jamenmarz@gmail.com>
2016-05-22 17:39:52 -0600

    Remove request

--- packages/hmu-npm/index.js
+++ packages/hmu-npm/index.js
@@ -1,13 +1,21 @@
-var request = require('request');
+var request = require('http').request;
 var routine = require('promise-routine');
 var Promise = require('any-promise');
 
 module.exports = function npmPlugin(input) {
   var getStatus = function getStatus(name) {
     return new Promise(function(resolve) {
-      request('http://registry.npmjs.org/' + name, function(_, data) {
-        resolve(['npm', name, JSON.parse(data.body)._id ? 'taken' : 'free']);
-      });
+      request('http://registry.npmjs.org/' + name, function(resp) {
+        var collection = [];
+        resp.on('data', function(chunk) {
+          collection.push(chunk);
+        });
+
+        resp.on('end', function() {
+          var data = Buffer.concat(collection);
+          resolve(['npm', name, JSON.parse(data)._id ? 'taken' : 'free']);
+        });
+      }).end();
     });
   };
 
