f9bc223c4bbfee63fc2690859eae6ee5ba996106
Jamen Marzonie <jamenmarz@gmail.com>
2016-08-25 14:33:16 -0600

    Initial commit

---
+++ packages/email-router/index.js
@@ -0,0 +1,39 @@
+var router = function router(options) {
+  options = options || {};
+  var resolve = options.resolve || router.resolve;
+
+  var addressAlign = [];
+  var routes = [];
+
+  var route = function route(address, args) {
+    address = resolve(address);
+
+    if (!address || addressAlign.indexOf(address) === -1) {
+      address = options.default;
+    }
+
+    var offset = -1;
+    for (;;) {
+      offset = addressAlign.indexOf(address, offset + 1);
+      if (offset < 0) break;
+      routes[offset].apply(route, args);
+    }
+  };
+
+  route.on = function on(address, handler) {
+    address = resolve(address);
+    addressAlign.push(address);
+    routes.push(handler);
+  };
+
+  return route;
+};
+
+// Default email resolving
+router.resolve = function resolve(address) {
+  address = address.split('.').join('');
+  address = address.slice(0, address.lastIndexOf('+')) || address;
+  return address;
+};
+
+module.exports = router;
