a4af61d77d2dad2675181a567e25b87601904dce
Jamen Marzonie <jamenmarz@gmail.com>
2016-08-31 00:04:43 -0600

    Use parseFloat and some magic afterwards. (Closes #1)

--- packages/css-truncate-values/index.js
+++ packages/css-truncate-values/index.js
@@ -8,8 +8,6 @@ module.exports = normalize;
   *
   */
 
-var NUMBER = '1234567890.';
-
 function normalize(value) {
   // Run this function for each item if value is array.
   if (value instanceof Array) {
@@ -18,43 +16,11 @@ function normalize(value) {
     return output;
   }
 
-  // Separate value.
   var number = strip(value);
   var unit = value.slice(number.length);
-
-  // Normalize the number.
-  var i = -1;
-  var output = ''
-  var trail = '';
-  var capturing = 1;
-  var max = number.length;
-  while (++i < max) {
-    if (capturing === 1 && number[i] === '0') continue;
-    if (number[i] === '-' && output[0] !== '-') {
-      output += '-';
-      continue;
-    }
-    if (number[i] === '.') {
-      if (capturing === 2) return null;
-      capturing = 2;
-    }
-    if (capturing === 2) {
-      if (number[i] === '0') {
-        trail += '0';
-        continue;
-      }
-      output += trail;
-      trail = '';
-    }
-    if (NUMBER.indexOf(number[i]) !== -1) {
-      if (capturing === 1) capturing = 0;
-      output += number[i];
-    }
-  }
-
-  if (output[output.length - 1] === '.') output = output.slice(0, -1);
-  if (output[0] === '-' && output.length === 1) output = '0'
-
-  // Return normalized value.
-  return output ? output + unit : null;
+  number = parseFloat(number).toString();
+  var extra = number.indexOf('0.');
+  if (extra === 0) return number.slice(1) + unit;
+  if (extra === 1 && number[0] === '-') return '-' + number.slice(2) + unit;
+  return number + unit;
 };
--- packages/css-truncate-values/test.js
+++ packages/css-truncate-values/test.js
@@ -2,7 +2,7 @@ var normalize = require('./');
 var test = require('tape');
 
 test('leading zeroes', function(t) {
-  t.is(normalize('00001px'), '1px', 'normalize("1000px") === "1px"');
+  t.is(normalize('00001px'), '1px', 'normalize("0001px") === "1px"');
   t.is(normalize('0.0001em'), '.0001em', 'normalize("0.0001em") === ".0001em"');
   t.is(normalize('-003%'), '-3%', 'normalize("-003%") === "-3%"');
   t.is(normalize('-0.01kHz'), '-.01kHz', 'normalize("-0.01kHz") === "-.01kHz"');
@@ -15,6 +15,15 @@ test('trailing zeroes', function(t) {
   t.is(normalize('-1.00100em'), '-1.001em', 'normalize("-1.00100em") === "-1.001em"');
   t.is(normalize('.050s'), '.05s', 'normalize(".050s") === ".05s"');
   t.is(normalize('1.000s'), '1s', 'normalize("1.000s") === "1s"');
+  t.end();
+});
+
+test('multiple values', function(t) {
+  t.same(
+    normalize(['001px', '-1.010em', '.050s', '-0.0']),
+    ['1px', '-1.01em', '.05s', '0'],
+    'mix of values'
+  );
   t.end();
 });
 
