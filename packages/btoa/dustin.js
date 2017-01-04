898e8ed10ffa0014f82bb201a1b841376cf4b4d1
Dustin Dowell <dustindowell22@gmail.com>
2017-01-03 20:51:53 -0600

    dustin.js

--- packages/btoa/dustin.js
+++ packages/btoa/dustin.js
@@ -1,9 +1,8 @@
 module.exports = function (v) {
-  var l=v.length,i=(~~(l/3)+1)*4,e=l,n,c='',s,u=0,r='',m='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/',t
-    while(--i){
-      if(e-->0)n=v[l-e-1].charCodeAt().toString(2),c=c+('00000000'.substr(n.length)+n);s=c.substr(u,6);r=r+m[parseInt(s,2)];u=u+6
-    }
-    t=s.length
-    return r+(t<7?'==':t<8?'=':'')
+  for(var l=v.length,i=(~~(l/3)+1)*4,e=l,n,c='',s,u=0,r='',m='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/',t;i>1;--i){
+    if(e-->0)n=v[l-e-1].charCodeAt().toString(2),c=c+('00000000'.substr(n.length)+n)
+    s=c.substr(u,6);r=r+m[parseInt(s,2)];u=u+6
   }
+  t=s.length
+  return r+(t<7?'==':t<8?'=':'')
 }
