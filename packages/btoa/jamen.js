162ccd748c60ac6c95e4a3decd55ead64cbcd92c
Jamen Marz <jamenmarz@gmail.com>
2017-01-03 23:24:35 -0700

    Update my version

--- packages/btoa/jamen.js
+++ packages/btoa/jamen.js
@@ -1,2 +1,2 @@
 var n='',m='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/',a=n.charCodeAt.call.bind(n.charCodeAt);
-module.exports=function btoa(s){for(var i=0,q=s.length,r=n,x,y,z;i<q;x=s[i++],x=x&&a(x),y=s[i++],y=y&&a(y),z=s[i++],z=z&&a(z),r+=(x?m[x>>2]:n)+(x?m[((x&3)<<4)+(y>>4)]:n)+(y?m[((y&15)<<2)+(z>>6)]:'=')+(z?m[z&63]:'='));return r}
+module.exports=function btoa(s){for(var i=0,q=s.length,r=n,w,x,y;i<q;w=(a(s,i++)<<16)+((x=a(s,i++))<<8)+((y=a(s,i++))||0),r+=(m[w>>18]||n)+(m[(w&258048)>>12]||n)+(x?m[(w&4032)>>6]:'=')+(y?m[w&63]:'='));return r}
