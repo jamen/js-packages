5641a989d1d13777c1deb6eda937314a238a25eb
Dustin Dowell <dustindowell22@gmail.com>
2017-01-04 00:08:50 -0600

    quotes

--- packages/btoa/test.js
+++ packages/btoa/test.js
@@ -5,7 +5,7 @@ var official = require('btoa')
 function doTests (t, btoa) {
   t.plan(4)
 
-  var full = `Man is distinguished, not only by his reason, but by this singular passion from other animals, which is a lust of the mind, that by a perseverance of delight in the continued and indefatigable generation of knowledge, exceeds the short vehemence of any carnal pleasure.`
+  var full = 'Man is distinguished, not only by his reason, but by this singular passion from other animals, which is a lust of the mind, that by a perseverance of delight in the continued and indefatigable generation of knowledge, exceeds the short vehemence of any carnal pleasure.'
 
   t.is(btoa(full), 'TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvbmx5IGJ5IGhpcyByZWFzb24sIGJ1dCBieSB0aGlzIHNpbmd1bGFyIHBhc3Npb24gZnJvbSBvdGhlciBhbmltYWxzLCB3aGljaCBpcyBhIGx1c3Qgb2YgdGhlIG1pbmQsIHRoYXQgYnkgYSBwZXJzZXZlcmFuY2Ugb2YgZGVsaWdodCBpbiB0aGUgY29udGludWVkIGFuZCBpbmRlZmF0aWdhYmxlIGdlbmVyYXRpb24gb2Yga25vd2xlZGdlLCBleGNlZWRzIHRoZSBzaG9ydCB2ZWhlbWVuY2Ugb2YgYW55IGNhcm5hbCBwbGVhc3VyZS4=', 'full text')
   t.is(btoa('any carnal pleasure.'), 'YW55IGNhcm5hbCBwbGVhc3VyZS4=', 'padding 1')
